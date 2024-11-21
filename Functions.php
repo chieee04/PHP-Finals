64424c7ab854c822fc9ffca793621fd4e83e1aef
chieee04 <harzwellacson@gmail.com>
2024-11-22 02:48:56 +0800

    SuccessfullyAddedRequiredDesign/Logout/gaurd

--- Functions.php
+++ Functions.php
@@ -1,9 +1,70 @@
 <?php
 
+DEFINE("DB_SERVER", "localhost");
+DEFINE("DB_USERNAME", "root");
+DEFINE("DB_PASSWORD", "");
+DEFINE("DB_NAME", "dct-ccs-finals");
 // Start the session if not already started
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
+function openCon(){
+    $con = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+
+    if ($con == false) {
+        die("ERROR: Could not connect" . mysqli_connect_error());
+        return $con;
+    }
+}
+
+function CloseCon(){
+    mysqli_close($con);
+}
+
+function getRec($con,$strSql){
+    $arrRec = [];
+    $i = 0;
+    if ($rs = mysqli_query($con,$strSql)) {
+        if (mysqli_num_rows($rs)== 1) {
+            $rec = mysqli_fetch_array($rs);
+            foreach ($rec as $key=> $value) {
+                $arrRec[$key] = $value;
+            }
+        }
+        elseif(mysqli_num_rows($rs)> 1){
+            while ($rec = mysqli_fetch_array($rs)) {
+               foreach ($rec as $key=> $value) {
+                $arrRec[$i][$key] = $value;
+                }
+                $i++;
+            }
+        }
+        mysqli_free_result($rs);
+    }
+    else
+        die("ERROR: Could not connect");
+    return $arrRec;
+}
+
+function exeuteInsertLastQuery($con,$strSql){
+    if (mysqli_query($con,$strSql)) {
+        return mysql_insert_id($con);
+    }
+    else
+        return 0;
+}
+function sanitize($con,$input){
+    return mysql_real_escape_string($con, stripcslashes(htmlspecialchars($input)));
+}
+
+
+
+
+
+
+
+
+
 
 // Function to validate email
 function validateEmail($email) {
